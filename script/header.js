60f2db3596f98c102016fcec3aa63176deb51c90
Konradpina <123588475+Konradpina@users.noreply.github.com>
2023-06-03 20:25:10 +0200

    Add files via upload

---
+++ script/header.js
@@ -0,0 +1,10 @@
+
+var more=document.querySelector(".more")
+more.onclick =function(){
+    var navbar= document.querySelector(".navbar")
+    navbar.classList.toggle("active")
+}
+function changeh(){
+    var navbar= document.querySelector(".navbar")
+    navbar.classList.toggle("active")
+}
